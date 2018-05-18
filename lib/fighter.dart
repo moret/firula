f515d3f303ed8b52b69d30724f060cc0cc2eea9c
Danilo Moret <github@moret.pro.br>
2018-05-18 10:48:59 -0300

    Move Fighter to its own component

---
+++ lib/fighter.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Fighter extends StatefulWidget {
+  Fighter({Key key, this.name}) : super(key: key);
+
+  final String name;
+
+  @override
+  _FighterState createState() => new _FighterState();
+}
+
+class _FighterState extends State<Fighter> {
+  int _powerLevel = 0;
+
+  void _decrementCounter() {
+    setState(() {
+      _powerLevel = max(0, _powerLevel - 3000);
+    });
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _powerLevel += 1234;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new Text(
+          (_powerLevel > 9000) ? 'It\'s over nine thousaaands!' : 'Pathetic.'
+        ),
+        new Text(
+          '$_powerLevel',
+          style: Theme.of(context).textTheme.display1,
+        ),
+        new RaisedButton(
+          child: new Text(
+            '+'
+          ),
+          onPressed: _incrementCounter,
+        ),
+        new RaisedButton(
+          child: new Text(
+            '-'
+          ),
+          onPressed: _decrementCounter,
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'fighter.dart';
 
 void main() => runApp(new MyApp());
 
@@ -11,65 +11,12 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.yellow,
       ),
-      home: new MyHomePage(title: 'Firula Counter'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _powerLevel = 0;
-
-  void _decrementCounter() {
-    setState(() {
-      _powerLevel = max(0, _powerLevel - 3000);
-    });
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _powerLevel += 1234;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              (_powerLevel > 9000) ? 'It\'s over nine thousaaands!' : 'Pathetic.'
-            ),
-            new Text(
-              '$_powerLevel',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            new RaisedButton(
-              child: new Text(
-                '+'
-              ),
-              onPressed: _incrementCounter,
-            ),
-            new RaisedButton(
-              child: new Text(
-                '-'
-              ),
-              onPressed: _decrementCounter,
-            ),
-          ],
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Firula Counter'),
+        ),
+        body: new Center(
+          child: new Fighter(),
         ),
       ),
     );
